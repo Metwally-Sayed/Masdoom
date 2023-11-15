d9c3f469317b8aa602c0639b6bbb9dd4008c591c
Metwally sayed <metwallysayed1999@gmail.com>
2023-11-15 02:12:58 +0200

    fixing me auth route

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -13,9 +13,8 @@ export async function GET(req: NextRequest) {
     const validToken = jwt.verify(currentToken, process.env.TOKEN_SECRET!);
 
     if (!validToken) {
-      cookiesStore.delete("token");
       const response = NextResponse.json(
-        { message: "!token not vaild" },
+        { message: "token not vaild" },
         { status: 401 }
       );
       return response;
@@ -29,6 +28,7 @@ export async function GET(req: NextRequest) {
 
     return response;
   } catch (error) {
+    console.log(error);
     return NextResponse.json({ error }, { status: 500 });
   }
 }
